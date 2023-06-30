3f6548d85b2cee55bdd49536eec9c0d7d12801e2
dil-gfazekas <gfazekas@diligent.com>
2023-06-30 12:04:57 +0200

    check local storage of banking script

--- cypress/e2e/banking.spec.cy.js
+++ cypress/e2e/banking.spec.cy.js
@@ -1,26 +1,28 @@
+const moment = require('moment');
+
 describe('Banking Project', () => {
   it('Logs in as customer "Harry Potter"', () => {
-    // Open the page
     cy.visit('https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login');
 
-    // Find and click the "Customer Login" button
     cy.contains('button', 'Customer Login').click();
 
-    // Find and select the customer "Harry Potter" from the dropdown
     cy.get('#userSelect').select('Harry Potter');
 
-    // Find and click the "Login" button
     cy.contains('button', 'Login').click();
 
-    // Assert that the user is logged in by verifying the presence of the greeting message
     cy.contains('span.fontBig.ng-binding', 'Harry Potter').should('be.visible');
 
-    deposit(100);
-    deposit(10);
-    deposit(5);
+    cy.get('option[selected="selected"]').invoke('prop', 'value').then(value => {
+      let accountNumber = value.split('number:')[1];
+      return accountNumber;
+    }).then((accountNumber) => {
+      deposit(100, accountNumber);
+      deposit(10, accountNumber);
+      deposit(5, accountNumber);
+    });
 
-    cy.get('div.center strong.ng-binding:nth-child(2)').should('have.text',"115");
-    cy.wait(2000)
+    cy.get('div.center strong.ng-binding:nth-child(2)').should('have.text', "115");
+    cy.wait(2000);
     cy.get('button[ng-class="btnClass1"]').click();
     cy.get('tbody').should('be.visible');
 
@@ -31,19 +33,68 @@ describe('Banking Project', () => {
       if (!isNaN(number)) {
         sum += number;
       }
-    }).then(()=>{
-      expect(sum).to.be.eq(115)
+    }).then(() => {
+      expect(sum).to.be.eq(115);
     });
   });
 });
 
-function deposit(value){
+function deposit(value, accountNumber) {
   cy.get('button[ng-class="btnClass2"]').click();
 
   cy.get('input[placeholder="amount"]').type(value);
 
   cy.get('button[type="submit"]').click();
 
-  cy.get('span.error').should('contain',"Deposit Successful")
+  cy.get('span.error').should('contain', "Deposit Successful");
 
+  checkLocalStorageForValue(value, accountNumber);
+}
+
+function checkLocalStorageForValue(value, accountNumber) {
+  cy.window().then((win) => {
+    const localStorageValue = win.localStorage.getItem('Transaction');
+
+    const parsedValue = JSON.parse(localStorageValue);
+
+    const dateToFind = new Date();
+    const valueDate = moment(dateToFind);
+
+    const keyToFind = accountNumber;
+
+    const valueToFind = {
+      date: valueDate.format('DD-MM-YYYY'),
+      amount: value
+    };
+
+    const result = findObject(parsedValue, keyToFind, valueToFind);
+    if (result === null) {
+      throw new Error('Object not found.');
+    }
+  });
+}
+
+function findObject(json, key, value) {
+  if (typeof json !== 'object' || json === null) {
+    return null;
+  }
+
+  for (const k in json) {
+    if (k == key && Array.isArray(json[k])) {
+      for (const obj of json[k]) {
+        const objDate = moment(obj.date);
+        const objFormattedDate = objDate.format('DD-MM-YYYY');
+        if (objFormattedDate === value.date && obj.amount === value.amount) {
+          return obj;
+        }
+      }
+    }
+
+    const result = findObject(json[k], key, value);
+    if (result !== null) {
+      return result;
+    }
+  }
+
+  return null;
 }
